e2fcc45c545643e8b95451e1856185c30a886d87
onfranciis <onukwuf@gmail.com>
2023-09-10 20:34:08 +0100

    feat: add CRUD routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import bodyParser from "body-parser";
 import express from "express";
+import CreateUser from "./controllers/CreateUser.controller";
+import ReadUser from "./controllers/ReadUser.controller";
+import UpdateUser from "./controllers/UpdateUser.controller";
+import DeleteUser from "./controllers/DeleteUser.controller";
 
 const app = express();
 
@@ -8,5 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send({ connected: true });
 });
+
+app.post("/api/create/", CreateUser);
+app.get("/api/read/:name", ReadUser);
+app.patch("/api/update/", UpdateUser);
+app.delete("/api/delete/", DeleteUser);
 
 export default app;
